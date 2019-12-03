f674a3de051db3f068eb94de765a045bd03c95b2
Oky Aneka <okyaneka@gmail.com>
2019-12-03 11:03:24 +0700

    Make unyil model

---
+++ app/Http/Controllers/UnyilController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\unyil;
+use Illuminate\Http\Request;
+
+class UnyilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\unyil  $unyil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(unyil $unyil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\unyil  $unyil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(unyil $unyil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\unyil  $unyil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, unyil $unyil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\unyil  $unyil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(unyil $unyil)
+    {
+        //
+    }
+}
---
+++ database/factories/UnyilFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\unyil;
+use Faker\Generator as Faker;
+
+$factory->define(unyil::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
